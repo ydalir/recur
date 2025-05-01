d08258a87a8488c01d8f8bd9a76c7d4135b50bc1
Ferdinand Løberg <ferdloeb@gmail.com>
2025-05-01 19:14:20 +0200

    Add header

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,12 +1,16 @@
 import { NavLink, Outlet } from "react-router";
+import style from "./Navigation.module.css";
 
 export const Navigation = () => {
   return (
     <>
-      <nav>
-        <NavLink to={"/"}>Log</NavLink>
-        <NavLink to={"/tasks"}>Tasks</NavLink>
-      </nav>
+      <div className={style.header}>
+        <h1>Recur</h1>
+        <nav className={style.nav}>
+          <NavLink to={"/"}>Log</NavLink>
+          <NavLink to={"/tasks"}>Tasks</NavLink>
+        </nav>
+      </div>
       <Outlet />
     </>
   );
