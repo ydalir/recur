785f6a4bd8eccfe03fa63c91d75cf8a69089fe16
Ferdinand Løberg <ferdloeb@gmail.com>
2025-05-01 19:02:07 +0200

    Set reset.css and font

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App.tsx";
-import "./global.css";
+import "./style/global.css";
+import "./style/reset.css";
 import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById("root")!).render(
