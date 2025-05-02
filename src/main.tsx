7c61ffdfe97fa858c1861704165137f568645f61
Ferdinand Løberg <ferdloeb@gmail.com>
2025-05-02 21:24:52 +0200

    Try adding base name

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="recur">
       <App />
     </BrowserRouter>
   </StrictMode>
